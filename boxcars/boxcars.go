cb5575c596248f5996201e30959292b7dca66400
Stephen Coward <stephendcoward@gmail.com>
2013-10-02 03:09:42 -0500

    Changing repo locations

--- boxcars/boxcars.go
+++ boxcars/boxcars.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/azer/boxcars"
+	"github.com/scoward/boxcars"
 	"os"
 )
 
--- debug.go
+++ debug.go
@@ -1,7 +1,7 @@
 package boxcars
 
 import (
-	. "github.com/azer/debug"
+	. "github.com/scoward/debug"
 )
 
 var debug = DebugScope("boxcars", "sites", "server", "auto-reload", "config", "secure")
